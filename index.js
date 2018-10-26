4f3707131506193e3b164ef12eb5657f19eb69cd
Eva <evaliisagalen@gmail.com>
2018-10-25 17:43:52 -0700

    changed App import path to new file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /** @format */
 import {AppRegistry} from 'react-native';
-import App from './src/screens/App';
+import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
